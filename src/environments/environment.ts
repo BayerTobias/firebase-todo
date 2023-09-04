3ab874df150764bbe81b261b9e9f3a1053333346
BayerTobias <tbayer2@gmx.de>
2023-09-04 20:19:57 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'tbayerfirebasetodo',
+    appId: '1:936301631068:web:9f98b90cbca91c45cab899',
+    storageBucket: 'tbayerfirebasetodo.appspot.com',
+    apiKey: 'AIzaSyDXIr3lkt2Yu9ZyUpWjm4frD2gB1Zb3REs',
+    authDomain: 'tbayerfirebasetodo.firebaseapp.com',
+    messagingSenderId: '936301631068',
+  },
+};
