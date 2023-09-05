efc67c015d307841f3b4d927fb25e92750e045d5
BayerTobias <tbayer2@gmx.de>
2023-09-05 20:30:28 +0200

    recive and send data added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { inject } from '@angular/core';
+import {
+  Firestore,
+  collectionData,
+  collection,
+  setDoc,
+  doc,
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'firebase-todo-list';
+  todos$: Observable<any>;
+  firestore: Firestore = inject(Firestore);
+
+  todos: Array<any>;
+  todoText: string;
+
+  constructor() {
+    const itemCollection = collection(this.firestore, 'todos');
+    this.todos$ = collectionData(itemCollection);
+
+    this.todos$.subscribe((newTodos) => {
+      this.todos = newTodos;
+    });
+  }
+
+  addTodo() {
+    const coll = collection(this.firestore, 'todos');
+    setDoc(doc(coll), { name: this.todoText });
+  }
 }
